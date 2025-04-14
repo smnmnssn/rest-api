6c877634342d90caffd5c63d64437c10d01d9303
Simon <simonmansson@live.se>
2025-04-14 18:17:04 +0200

    delete and update

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,15 +1,64 @@
 import express from "express";
 
-import { getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 
-export const getAllUsers = async (req: express.Request, res:express.Response) => {
-    try {
-        const users = await getUsers();
+export const getAllUsers = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const users = await getUsers();
 
-        return res.status(200).json(users);
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
 
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(400);
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    console.log("Delete route triggered");
+    console.log("Params:", req.params);
+    console.log("Headers:", req.headers);
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+
+    return res.status(200).json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(403);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};

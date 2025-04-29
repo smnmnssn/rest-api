0b3bbc34842108821046a19845ec410119fbe925
Simon <simonmansson@live.se>
2025-04-29 12:14:17 +0200

    changed order

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,39 @@ import express from "express";
 import { createUser, getUserByEmail } from "../db/users";
 import { authentication, random } from "../helpers";
 
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res.sendStatus(400).json({ error: "Missing required fields" });
+    }
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return res.sendStatus(400).json({ error: "User already exists" });
+    }
+
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res
+      .sendStatus(400)
+      .json({ error: "Registration failed", details: error });
+  }
+};
+
 export const login = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password } = req.body;
@@ -45,35 +78,4 @@ export const login = async (req: express.Request, res: express.Response) => {
   }
 };
 
-export const register = async (req: express.Request, res: express.Response) => {
-  try {
-    const { email, password, username } = req.body;
 
-    if (!email || !password || !username) {
-      return res.sendStatus(400).json({ error: "Missing required fields" });
-    }
-
-    const existingUser = await getUserByEmail(email);
-
-    if (existingUser) {
-      return res.sendStatus(400).json({ error: "User already exists" });
-    }
-
-    const salt = random();
-    const user = await createUser({
-      email,
-      username,
-      authentication: {
-        salt,
-        password: authentication(salt, password),
-      },
-    });
-
-    return res.status(200).json(user).end();
-  } catch (error) {
-    console.log(error);
-    return res
-      .sendStatus(400)
-      .json({ error: "Registration failed", details: error });
-  }
-};
